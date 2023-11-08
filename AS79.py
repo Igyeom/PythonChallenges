78dfc8361bd546329705850e520385a38a45eb21
Ian <61957594+Igyeom@users.noreply.github.com>
2023-11-08 14:52:14 +0800

    Add files via upload

---
+++ AS79.py
@@ -0,0 +1,7 @@
+def make_it_laugh(string):
+    return string.replace("a", "haha").replace("A", "haha").replace("e", "haha").replace("E", "haha").replace("i", "haha").replace("I", "haha").replace("o", "haha").replace("o", "haha").replace("u", "haha").replace("U", "haha")
+
+print("Hello, World!")
+print(make_it_laugh("Hello, World!"))
+print("FIZZBUZZ")
+print(make_it_laugh("FIZZBUZZ"))
