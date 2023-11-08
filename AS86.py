578eef6d5e868496f49ba6fdda82e254a42081de
Ian <61957594+Igyeom@users.noreply.github.com>
2023-11-08 16:59:22 +0800

    Create AS86.py

---
+++ AS86.py
@@ -0,0 +1,2 @@
+with open("promptspal.txt","r") as f:print(__import__("random").choice(f.readlines()))
+# one-liner that requires this file: https://drive.google.com/file/d/1MJSZIJwO35OikSoyzOehyDYlVZq7audC/view (86B/86C)
