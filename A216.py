442def172513c8727330ba1bbfbbf65c55027dae
Ian <61957594+Igyeom@users.noreply.github.com>
2023-11-08 17:13:43 +0800

    Update A216.py

--- A216.py
+++ A216.py
@@ -12,3 +12,5 @@ for p in permutations(word):
         total += 1
 
 print(total-1, "anagram(s) found [note: there are duplicates in the word list given by Mr. Abela]")
+
+# may need this word list: https://gist.githubusercontent.com/jamesabela/d956da0889582e5c5c8fc34ed72b97c3/raw/5a559a4efeb4050438c52c5b3a919d5065de51a8/wordlist.txt
