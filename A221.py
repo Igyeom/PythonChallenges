15592b547009ed9071349633a1d3f33263eaa665
Ian <61957594+Igyeom@users.noreply.github.com>
2023-11-08 17:55:22 +0800

    Create A221.py

---
+++ A221.py
@@ -0,0 +1,3 @@
+def r(arr):return[*set(arr)]
+# print(remove_duplicates([1, 1, 1, 2, 3, 4, 4, 4, 5, 6, 6]))
+# Not the most descriptive function name, I know. But I'm going for the shortest code here. (28B/28C)
