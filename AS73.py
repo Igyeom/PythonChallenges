7a13ffd36fe6cd44080b19b714e0884ec7dc90e6
Ian <61957594+Igyeom@users.noreply.github.com>
2023-11-12 07:36:56 +0800

    Create AS73.py

---
+++ AS73.py
@@ -0,0 +1,3 @@
+colour = input("Favourite Colour: ");place = input("Favourite Place: ");animal = input("Favourite Animal: ");print(f"Password: {animal}$!{place}AND&{animal}$!@")
+
+# crazy one-liner lol
