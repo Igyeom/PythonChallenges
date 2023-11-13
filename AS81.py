1a86b1a8b51b7892fd4f1a3733eab5d0c3bc3a12
Ian <61957594+Igyeom@users.noreply.github.com>
2023-11-13 19:26:04 +0800

    Create AS81.py

---
+++ AS81.py
@@ -0,0 +1,3 @@
+a=input().split();print(sum(map(int,a)),eval('*'.join(a)))
+
+# one-liner that takes a space-separated list of integers and completes the procedure, with the output being the sum and product respectively. (58B/58C)
