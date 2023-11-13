04fb24aaee52eca9d5f328a1f87f7a453ef8a19d
Ian <61957594+Igyeom@users.noreply.github.com>
2023-11-13 20:14:41 +0800

    Create  GC09.py

---
+++ GC09.py
@@ -0,0 +1,3 @@
+print(' '.join([f"{i} bottles of cola on the wall. {i} bottles of cola, take one down, pass it around." for i in range(99,2,-1)]) + ' No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.')
+
+# crazy long one-liner. who cares about PEP 8 anyway lol (264B/264C)
