c2fa17f3e235d7a323413d11ecee44ce9f379142
Ian <61957594+Igyeom@users.noreply.github.com>
2024-03-04 11:36:33 +0800

    Create A261.py

---
+++ A261.py
@@ -0,0 +1,11 @@
+import chess
+import chess.engine
+
+
+engine = chess.engine.SimpleEngine.popen_uci("stockfish-mv")
+board = chess.Board()
+
+while True:
+  result = engine.play(board, chess.engine.Limit(time=0.05))
+  print(board)
+  board.push_san(input("Your move: "))
