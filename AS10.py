37a04ed83f447fb00b561946d5b47466fce5ae33
Ian <61957594+Igyeom@users.noreply.github.com>
2024-07-16 14:18:44 +0900

    Create AS10.py

---
+++ AS10.py
@@ -0,0 +1,14 @@
+s=""
+ans=0
+while s!="oiuroieurpyejuvm":
+  s=input()
+  if "ab" in s or "cd" in s or "pq" in s or "xy" in s: continue
+  elif sum([set(s).count(i) for i in "aeiou"]) >= 3:
+    for i in range(1, len(s)):
+      if s[i-1] == s[i]: break
+    else:
+      continue
+    ans += 1
+
+
+# note: you must feed the input file into stdin (standard input)
