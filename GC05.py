4351bc911c85a6accbaf694527691f14d1287b87
Ian <61957594+Igyeom@users.noreply.github.com>
2024-12-31 07:27:04 +0800

    Create GC05.py

---
+++ GC05.py
@@ -0,0 +1,19 @@
+# Sequence
+natural_seq = [1, 2, 3, 4, 5, 6]
+even_seq = [2, 4, 6, 8, 10]
+
+# Selection
+if int(input())%2==0:
+    print("Even")
+else:
+    print("Odd")
+
+# Repetition
+for i in natural_seq:
+    print(i)
+
+# Variable
+k = 1
+print(k)
+k += 3
+print(k)
