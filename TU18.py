b21ba5b1502ecefdd724fe0897f1e10eb1187356
Ian <61957594+Igyeom@users.noreply.github.com>
2024-12-30 08:33:35 +0800

    Create TU18.py

---
+++ TU18.py
@@ -0,0 +1,11 @@
+input("Start? (press enter to continue)")
+with open("rudyard.txt","r") as whole_file:
+   for line in whole_file:
+        print(line.strip("\n"),end="")
+        input()
+
+input("Start? (press enter to continue)")
+with open("mam.txt","r") as whole_file:
+   for line in whole_file:
+        print(line.strip("\n"),end="")
+        input()
