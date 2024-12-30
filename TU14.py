7a1e3cd50f16092b979b202f0a1b2d9fcc03c197
Ian <61957594+Igyeom@users.noreply.github.com>
2024-12-30 08:17:41 +0800

    Create TU14.py

---
+++ TU14.py
@@ -0,0 +1,16 @@
+# Exercise 1
+MyString = "abcdef"
+print(MyString)
+
+# Exercise 2
+s = "hello world"
+print("Original string:")
+print(s)
+print("Skipping first character:")
+print(s[1:])
+print("Skipping last character:")
+print(s[:-1])
+print("Skipping every other character:")
+print(s[::2])
+print("Reversed string (via slicing):")
+print(s[::-1])
