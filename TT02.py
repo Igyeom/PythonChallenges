df6689e2743b07388dd1e160be4588d5e8b4b46f
Ian <61957594+Igyeom@users.noreply.github.com>
2024-12-30 18:36:05 +0800

    Create TT02.py

---
+++ TT02.py
@@ -0,0 +1,7 @@
+import turtle
+j=input("What would you like me to write?")
+turtle.write(j)
+turtle.forward(5*len(j))
+turtle.shape("turtle")
+turtle.done()
+# same code as TT01 as i had already implemented that functionality into the code for that challenge.
