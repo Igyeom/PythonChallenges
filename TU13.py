e84c0fc3a7c1631c0d5545ba00a4c9d5b008a1a3
Ian <61957594+Igyeom@users.noreply.github.com>
2024-12-30 08:14:04 +0800

    Create TU13.py

---
+++ TU13.py
@@ -0,0 +1,11 @@
+# Exercise 1
+MyList = [12,34,56,78,90]
+print(sum(MyList))
+
+# Exercise 2
+arr = [4398564, 4, 67, 423, 3, 4]
+print("Total:", sum(arr))
+print("Minimum:", min(arr))
+print("Maximum:", max(arr))
+print("Length:", len(arr))
+print("Mean:", sum(arr)/len(arr))
