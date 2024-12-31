31bd42f5a434c3f04b31f74d6190d487fcec7379
Ian <61957594+Igyeom@users.noreply.github.com>
2024-12-31 13:24:34 +0800

    Create GC26.py

---
+++ GC26.py
@@ -0,0 +1,6 @@
+s = input("Type the phrase 'peter piper picked a peck of pickled peppers', without quotes, all in lowercase.\n")
+s2 = input("Type the same phrase again.\n")
+if s == "peter piper picked a peck of pickled peppers" and s2 == "peter piper picked a peck of pickled peppers":
+    print("You are permitted to enter.")
+else:
+    print("You have failed the challenge.")
