a54682f87e2ff293eee814ed7a652876675ae7a1
Ian <61957594+Igyeom@users.noreply.github.com>
2025-01-01 14:52:42 +0800

    Create GC37.py

---
+++ GC37.py
@@ -0,0 +1,38 @@
+# Exercise 1
+word1 = "All"
+word2 = "work"
+word3 = "and"
+word4 = "no"
+word5 = "play"
+word6 = "makes"
+word7 = "Jack"
+word8 = "a"
+word9 = "dull"
+word10 = "boy."
+print(word1, word2, word3, word4, word5, word6, word7, word8, word9, word10)
+
+# Exercise 2
+print(6*(1-2))
+
+# Exercise 3 (commenting code makes the interpreter ignore it)
+#print("Hello")
+
+# Exercise 4
+bruce = 6
+print(bruce+4)
+
+# Exercise 5
+p = 10000
+n = 12 # what's this for??
+r = 1.08
+t = int(input("t = "))
+print(p*r**t)
+
+# Exercise 6
+# You cannot divide a number by 0.
+
+# Exercise 7
+print((14+51)%24) # 17:00
+
+# Exercise 8
+print((int(input("Time now in the 24-hour clock (hour): "))+int(input("Hours to wait: ")))%24)
