0ddbd110ebefedaf1283d6ef456656105eb0f2c7
Ian <61957594+Igyeom@users.noreply.github.com>
2025-01-04 08:41:51 +0800

    Update A265.py

--- A265.py
+++ A265.py
@@ -1,8 +1,12 @@
-print("""7
-1 3
-1 2
-3 2
-1 3
-2 1
-2 3
-1 3""")
+def solve(n, s, m, e):
+    if n == 1:
+        print(s, e)
+        return
+    solve(n-1, s, e, m)
+    print(s, e)
+    solve(n-1, m, s, e)
+
+n = int(input()) # enter 3 for three discs
+
+print(2**n-1)
+if n <= 20: solve(n, 1, 2, 3)
