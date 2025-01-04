97dd80d370084f551c715d594b6ed448dd389ac3
Ian <61957594+Igyeom@users.noreply.github.com>
2025-01-04 08:46:39 +0800

    Update A202.py

--- A202.py
+++ A202.py
@@ -1,6 +1,26 @@
-def print_list(node):
-    print("[",end="")
-    while node is not None:
-        print(node, end=", ")
-        node = node.next
-    print("]",end="")
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+def push_front(head, data):
+    node = Node(data)
+    node.next = head
+    return node
+
+def traverse(head):
+    ptr = head
+    while ptr:
+        print(ptr.data, end=' > ')
+        ptr = ptr.next
+    print(None)
+
+
+head = None
+head = push_front(head, 6)
+head = push_front(head, 5)
+head = push_front(head, 4)
+head = push_front(head, 3)
+head = push_front(head, 2)
+head = push_front(head, 1)
+traverse(head)
