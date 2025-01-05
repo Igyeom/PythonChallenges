a5252ce32228ca692a27c73df222067a52185c9d
Ian <61957594+Igyeom@users.noreply.github.com>
2025-01-05 08:46:55 +0800

    Update AS11.py

--- AS11.py
+++ AS11.py
@@ -1,3 +1,7 @@
-print(__import__("secrets").token_urlsafe(16))
+# print(__import__("secrets").token_urlsafe(16))
 
 # another simple one-liner (46B/46C)
+
+with open("passphrase.txt", "w") as f:f.write(__import__("secrets").token_urlsafe(16))
+
+# includes file handling (50 bonus points)
