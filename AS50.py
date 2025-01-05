bedb1b6dacc3d28bbf5ed7053381afa1f25d094d
Ian <61957594+Igyeom@users.noreply.github.com>
2025-01-05 08:11:13 +0800

    Create AS50.py

---
+++ AS50.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+conn = sqlite3.connect('Fastcars.sql')
+
+def add_car():
+    with conn:
+        NewCarName = input("Car Name: ")
+        NewCarSpeed = input("Top Speed of car: ")
+        conn.execute("INSERT INTO Cars(Name,TopSpeed) VALUES(?,?)", (NewCarName,NewCarSpeed))
+
+def del_car():
+    with conn:
+        DeleteCar = input("Car ID for deletion:")
+        cur = conn.execute("SELECT * FROM Cars WHERE id = ?", (DeleteCar,))
+        print(f"Are you sure you'd like to delete the following record?")
+        for row in cur:
+            print(row, end=' ')
+        print()
+        confirm = input("(y/n) ")
+        if confirm.lower() == "y":
+            conn.execute("DELETE FROM Cars WHERE id = ?", (DeleteCar,))
+            print("Successfully removed the record.")
+        else:
+            return
+
+del_car()
